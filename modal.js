69853f4c11b3147632d74837e9bb6a0d14459cb0
UNB - FAAMA - Itamar Matheus Honorio de Farias <itamar.farias@faama.edu.br>
2026-03-24 10:04:16 -0300

    Versão Finalizada do sitema de Gerenciamento de Lista de Filmes MyMovies

---
+++ modal.js
@@ -0,0 +1,58 @@
+const background = document.getElementById("modal-background");
+const container = document.getElementById('modal-container')
+
+let currentMovie = {};
+
+function backgroundClickHandler() {
+  overlay.classList.remove("open");
+}
+
+function closeModal(){
+  overlay.classList.remove('open');
+}
+
+function addCurrentMovieToList(){
+  if(isMovieAlReadyOnList(currentMovie.imdbID)){
+    notie.alert({type: "error", text: "Filme já está na sua lista!"});
+    return;
+  }
+  addToList(currentMovie);
+  updateUI(currentMovie);
+  updateLocalStorage();
+  closeModal()
+}
+
+function createModal(data) {
+  currentMovie = data;
+
+  // Alteramos apenas o conteúdo de 'movieDataContainer', mantendo o botão seguro no HTML!
+  container.innerHTML = `
+    <h2 id="movie-title">${data.Title} - ${data.Year}</h2>
+    <section id="modal-body">
+      <img 
+        id="movie-poster" 
+        src="${data.Poster}" 
+        alt="Poster do Filme." 
+      />
+      <div id="movie-info">
+        <h3 id="movie-plot">${data.Plot}</h3>
+        <div id="movie-cast">
+          <h4>Elenco:</h4>
+          <h5>${data.Actors}</h5>
+        </div>
+        <div id="movie-genre">
+          <h4>Gênero:</h4>
+          <h5>${data.Genre}</h5>
+        </div>
+      </div>
+    </section>
+    <section id="modal-footer">
+       <button id="add-to-list">Adicionar à Lista</button>
+     </section>
+  `;
+
+  const addToListButton = document.getElementById("add-to-list");
+  addToListButton.addEventListener("click", addCurrentMovieToList);
+}
+
+background.addEventListener("click", backgroundClickHandler);
